0e49278e8e3908e6117e0843a5e7d7791a48f9f8
Tushar <tushar.vaid027@gmail.com>
2023-10-01 14:46:27 +0530

    Update Commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -55,13 +55,13 @@ export default function TextForm(props) {
                 <button disabled={text.length===0} className="btn btn-primary my-1" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
                 <button disabled={text.length===0} type="button" className="btn btn-danger mx-1 my-1" onClick={handleClearClick} >Clear Text</button>
-                <button disabled={text.length===0} type="button" className="btn btn-success mx-1 my-1" onClick={handleCopyClick}>Copy to Clipboard</button>
+                <button disabled={text.length===0} type="button" className="btn btn-success mx-1 my-1" onClick={handleCopyClick}>Copy Text</button>
                 <button disabled={text.length===0} type="button" className="btn btn-danger mx-1 my-1" onClick={handleSpaceClick}>Remove Extra Space</button>
                 <button disabled={text.length===0} style={{width:'90px'}} type="submit" onClick={speak} className="btn btn-warning mx-2 my-1" id="toggle">Speak</button>
             </div>
             <div className="container my-5">
                 <h2>Your Text Summary</h2>
-                <p>Word Count : {text.split(" ").filter((element)=>{return element.length!==0}).length}</p>
+                <p>Word Count : {text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</p>
                 <p>Character Count : {text.length}</p>
                 <p>Reading Time: {0.008 * text.split(" ").length} mins</p>
                 <h3>Preview</h3>
